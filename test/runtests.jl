16250fd04382ff44014b654b22ad7d962887cdcf
alahi639 <Alahi.JeeganahalliInayathbasha@ruhr-uni-bochum.de>
2024-10-09 15:17:12 +0200

    Implementing tests to check if the code works as expected

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a9691c20-863e-11ef-1811-f7b81f2ebdb3
+using DataAnalysisWS2425
+using Test
+@testset "Fake test" begin
+   @test 1 == 1
+end
+
+# ╔═╡ Cell order:
+# ╠═a9691c20-863e-11ef-1811-f7b81f2ebdb3
